198a55d89fba58cbe3b249dc1e0a2964790d09f1
Edwin Seclen <jtech3@dbperu.com>
2025-01-22 13:03:17 -0500

    Modiicación tabla detalle carga inventarios con angular material

--- src/app/Ui/Modules/Carga_Inventario/Page/detalle-carga-inventarios/detalle-carga-inventarios.component.ts
+++ src/app/Ui/Modules/Carga_Inventario/Page/detalle-carga-inventarios/detalle-carga-inventarios.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, SimpleChanges, ViewChild } from '@angular/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { detalleCarga } from 'src/app/Domain/models/cargaDatos/cargaDatos.model';
 import { inventariosModel } from 'src/app/Domain/models/inventarios/inventarios.models';
@@ -9,14 +11,52 @@ import { ButtonCerrarComponent } from 'src/app/Ui/Shared/Components/buttons/butt
   standalone: true,
   imports: [
     ButtonCerrarComponent,
-    NgxPaginationModule
+    NgxPaginationModule,
+    MatTableModule,
+    MatPaginatorModule
   ],
   templateUrl: './detalle-carga-inventarios.component.html',
   styleUrl: './detalle-carga-inventarios.component.css'
 })
 export class DetalleCargaInventariosComponent {
-  @Input() citaSeleccionada: inventariosModel = {} as inventariosModel;
+
+  displayedColumns: string[] = [
+    'almacen',
+    'sucursal',
+    'zona',
+    'pasillo',
+    'rack',
+    'ubicacion',
+    'esagrupado',
+    'codgrupo',
+    'codigoproducto',
+    'codigobarra',
+    'descripcionProducto',
+    'unidad',
+    'stockL'
+  ];
+
   @Input() detalleProductos: Array<detalleCarga> = []
+  dataSource = new MatTableDataSource<detalleCarga>([]);
+
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['detalleProductos']) {
+      this.dataSource.data = this.detalleProductos || [];
+    }
+  }
+
+
+
+
+  @Input() citaSeleccionada: inventariosModel = {} as inventariosModel;
+
   @Input() cantidadInventarios: number = 0
 
   @Output() changePage = new EventEmitter<number>();
@@ -29,4 +69,6 @@ export class DetalleCargaInventariosComponent {
   itemsPerPageProductos: number = 5;
   totalPagesProductos: number = 0;
   cantidadLlamarSoporte: number = 0;
+
+
 }
