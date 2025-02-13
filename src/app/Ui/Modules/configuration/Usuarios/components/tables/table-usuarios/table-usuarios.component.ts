589c4ff1deef8f23201be83144f08b043ed1b85f
seyalexander <alexande1809piscis@gmail.com>
2025-02-13 00:21:48 -0500

    13_02_2025 ADD PAGINADO LISTA USUARIOS

--- src/app/Ui/Modules/configuration/Usuarios/components/tables/table-usuarios/table-usuarios.component.ts
+++ src/app/Ui/Modules/configuration/Usuarios/components/tables/table-usuarios/table-usuarios.component.ts
@@ -6,6 +6,7 @@ import { HeaderTableUsuariosComponent } from '../../header-table-usuarios/header
 import { FooterComponent } from 'src/app/Ui/Shared/Components/organisms/footer/footer.component';
 import { DetalleUsuarioPageComponent } from '@modules/configuration/Usuarios/page/detalle-usuario-page/detalle-usuario-page.component';
 import { MensajeSeguridadModel } from 'src/app/Domain/models/seguridad/mensajeSeguridad.model';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'table-usuarios',
@@ -13,7 +14,8 @@ import { MensajeSeguridadModel } from 'src/app/Domain/models/seguridad/mensajeSe
   imports: [
     HeaderTableUsuariosComponent,
     FooterComponent,
-    DetalleUsuarioPageComponent
+    DetalleUsuarioPageComponent,
+    NgxPaginationModule
   ],
   templateUrl: './table-usuarios.component.html',
   styleUrl: './table-usuarios.component.css'
@@ -22,6 +24,7 @@ export class TableUsuariosComponent {
   private seguridadSubscription: Subscription | undefined;
   DatosUsuarios: Array<SeguridadModel> = [];
   cantidadUsuarios: number = 0
+  p: number = 1;
 
   ngOnInit(): void {
     this.listaUsuarios()
@@ -33,6 +36,14 @@ export class TableUsuariosComponent {
     .subscribe((response: MensajeSeguridadModel) => {
       this.DatosUsuarios = response.usuarios
     });
+  }
+
+
+  itemsPerPage: number = 10;
+
+  onItemsPerPageChange(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    this.itemsPerPage = Number(target.value);
   }
 
   constructor(private readonly _usuario: SeguridadService ){}
