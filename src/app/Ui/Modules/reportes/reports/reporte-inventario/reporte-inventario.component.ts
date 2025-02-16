b3d5b9da79dd37b286f3b139c32c57e850686837
seyalexander <alexande1809piscis@gmail.com>
2025-02-16 00:58:08 -0500

    16_02_2025 ADD BUSCAR TABLA REPORTE

--- src/app/Ui/Modules/reportes/reports/reporte-inventario/reporte-inventario.component.ts
+++ src/app/Ui/Modules/reportes/reports/reporte-inventario/reporte-inventario.component.ts
@@ -23,6 +23,8 @@ import { MatIcon } from '@angular/material/icon';
 import { MatSort } from '@angular/material/sort';
 import { MatMenu, MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'reporte-inventario',
@@ -33,6 +35,8 @@ import { MatButtonModule } from '@angular/material/button';
     MatIcon,
     MatMenu,
     MatMenuModule,
+    MatInputModule,
+    MatFormFieldModule,
     MatButtonModule,
     DescargarReporteExcelComponent,
     NgxPaginationModule,
@@ -79,6 +83,15 @@ export class ReporteInventarioComponent {
   // ---------------------------------------------------------------------------------------
   ngOnInit(): void {
     this.listarInventarios();
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
   // ---------------------------------------------------------------------------------------
