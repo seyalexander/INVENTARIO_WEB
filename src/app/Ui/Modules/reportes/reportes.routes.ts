e0e5a3a3b4f4cbc7bc0e972d8a97cba7593b83fc
Edwin Seclen <jtech3@dbperu.com>
2025-02-27 15:18:20 -0500

    27_02_2025 CAMBIO DISENI CARGA INVENTARIO Y REPORTES

--- src/app/Ui/Modules/reportes/reportes.routes.ts
+++ src/app/Ui/Modules/reportes/reportes.routes.ts
@@ -1,15 +1,9 @@
 import { Routes } from '@angular/router';
-import { ReportesPageComponent } from './page/reportes-page/reportes-page.component';
 
 export const REPORTES_ROUTES: Routes = [
   {
     path: '',
-    component: ReportesPageComponent,
-    children: [
-      {
-        path: '',
-        loadComponent: () => import('./reports/reporte-inventario/reporte-inventario.component').then(m=>m.ReporteInventarioComponent),
-      },
-    ]
+    loadComponent: () => import('./reports/reporte-inventario/reporte-inventario.component').then(m=>m.ReporteInventarioComponent)
+
   }
 ];
