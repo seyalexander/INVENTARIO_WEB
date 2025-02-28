89cce045be0b00665cac716fdcf18dc10b5b9552
Edwin Seclen <jtech3@dbperu.com>
2025-02-28 15:49:59 -0500

    28_02_2025 DELETE OPCOIN VER ALMACENES

--- src/app/Ui/Modules/configuration/Empresas/components/tables/table-lista-empresas/table-lista-empresas.component.ts
+++ src/app/Ui/Modules/configuration/Empresas/components/tables/table-lista-empresas/table-lista-empresas.component.ts
@@ -47,7 +47,7 @@ export class TableListaEmpresasComponent {
       'razonsocial',
       'fecharegistro',
       'Estado',
-      'opciones'
+
     ];
 
     dataSource = new MatTableDataSource<EmpresasModel>([]);
