51debf42a70601441d06a1242fee0779cb72fecc
seyalexander <alexande1809piscis@gmail.com>
2025-03-01 22:46:40 -0500

    01_03_2025 OPTIMIZACION CODIGO LISTA EMPRESAS

--- src/app/Ui/Modules/configuration/Empresas/components/tables/table-lista-empresas/table-lista-empresas.component.ts
+++ src/app/Ui/Modules/configuration/Empresas/components/tables/table-lista-empresas/table-lista-empresas.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { EmpresasService } from '../../../../../../../Infraestructure/driven-adapter/empresas/empresas.service';
 import { EmpresasModel } from '../../../../../../../Domain/models/empresas/empresas.model';
 import { Subscription } from 'rxjs';
@@ -7,7 +7,6 @@ import { BodyTableEstadoInactivoComponent } from '../../header/body-table/body-t
 import { DetalleEmpresaComponent } from '../../modals/detalle-empresa/detalle-empresa.component';
 import { MensajeResponseEmpresas } from 'src/app/Domain/models/empresas/ResponseEmpresas.model';
 import { RegistroEmpresaComponent } from '@modules/configuration/Empresas/page/registro-empresa/registro-empresa.component';
-import { NgxPaginationModule } from 'ngx-pagination';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatIconModule } from '@angular/material/icon';
@@ -29,7 +28,6 @@ import { MatInputModule } from '@angular/material/input';
     BodyTableEstadoActivoComponent,
     BodyTableEstadoInactivoComponent,
     DetalleEmpresaComponent,
-    NgxPaginationModule,
     MatTableModule,
     MatPaginatorModule,
     MatIconModule,
@@ -43,23 +41,21 @@ import { MatInputModule } from '@angular/material/input';
 export class TableListaEmpresasComponent {
 
   displayedColumns: string[] = [
-      'rucempresa',
-      'razonsocial',
-      'fecharegistro',
-      'Estado',
+    'rucempresa',
+    'razonsocial',
+    'fecharegistro',
+    'Estado',
+  ];
 
-    ];
+  dataSource = new MatTableDataSource<EmpresasModel>([]);
 
-    dataSource = new MatTableDataSource<EmpresasModel>([]);
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-    @ViewChild(MatPaginator) paginator!: MatPaginator;
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 
-    ngAfterViewInit() {
-      this.dataSource.paginator = this.paginator;
-    }
-
-
-
+  private readonly _empresas = inject(EmpresasService)
 
   private empresasSubscription: Subscription | undefined;
   private sucursalesSubscription: Subscription | undefined;
@@ -71,11 +67,6 @@ export class TableListaEmpresasComponent {
   DatosEmpresas: Array<EmpresasModel> = [];
   cantidadEmpresas: number = 0
   cantidadSucursales: number = 0
-  p: number = 1;
-
-  constructor(
-    private readonly _empresas: EmpresasService,
-  ) {}
 
   ngOnInit(): void {
     this.listaEmpresas()
@@ -84,55 +75,42 @@ export class TableListaEmpresasComponent {
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+    this.dataSource.paginator?.firstPage()
 
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
   }
 
   listaEmpresas() {
     this.empresasSubscription = this._empresas
-    .ListarEmpresas()
-    .subscribe((response: MensajeResponseEmpresas) => {
-      this.dataSource.data = response.empresas
-      this.cantidadEmpresas = response.empresas.length
+      .ListarEmpresas()
+      .subscribe((response: MensajeResponseEmpresas) => {
+        this.dataSource.data = response.empresas
+        this.cantidadEmpresas = response.empresas.length
 
-    });
+      });
   }
 
   DetalleEmpresas(rucEmpresa: string) {
     const reqDatos: RequestDetalleEmpresa = { rucEmpresa };
     this.empresaDetalleSubscription = this._empresas
-    .DetalleEmpresa(reqDatos)
-    .subscribe((response: EmpresasModel) => {
-      this.detalleEmpresa = response
-      const estado = response.estado
-      const rucEmpresa = response.rucempresa
-      this.ObtenerSucursales(rucEmpresa,estado)
-    });
+      .DetalleEmpresa(reqDatos)
+      .subscribe((response: EmpresasModel) => {
+        this.detalleEmpresa = response
+        const estado = response.estado
+        const rucEmpresa = response.rucempresa
+        this.ObtenerSucursales(rucEmpresa, estado)
+      });
   }
 
   ObtenerSucursales(rucempresa: string, estado: string) {
-    const reqSucursal: RequestObtenerSucursales = {estado,rucempresa }
-      this.sucursalesSubscription = this._empresas.ListaSucursales(reqSucursal).subscribe(
-        (response: ResponseObtenerSucursales) => {
-          this.sucursales = response.sucursales
-        }
-      )
+    const reqSucursal: RequestObtenerSucursales = { estado, rucempresa }
+    this.sucursalesSubscription = this._empresas.ListaSucursales(reqSucursal).subscribe(
+      (response: ResponseObtenerSucursales) => {
+        this.sucursales = response.sucursales
+      }
+    )
   }
-
-  itemsPerPage: number = 10;
-
-  onItemsPerPageChange(event: Event) {
-    const target = event.target as HTMLSelectElement;
-    this.itemsPerPage = Number(target.value);
-  }
-
   ngOnDestroy(): void {
-    if (this.empresasSubscription) {
-      this.empresasSubscription.unsubscribe();
-    }
-
+    this.empresasSubscription?.unsubscribe();
     this.empresaDetalleSubscription?.unsubscribe()
     this.sucursalesSubscription?.unsubscribe()
   }
