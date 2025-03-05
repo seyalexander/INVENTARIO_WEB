64a3e9b675e02c26359729e180489ad4bc5ed501
seyalexander <alexande1809piscis@gmail.com>
2025-03-05 14:41:28 -0500

    05_03_2025 ADD OPCION EN EL INICIO AGREGARDA EL AJUSTE DE INVENTARIO

--- src/app/Ui/Modules/Dashboard/Components/section-opciones/section-opciones.component.ts
+++ src/app/Ui/Modules/Dashboard/Components/section-opciones/section-opciones.component.ts
@@ -40,17 +40,17 @@ export class SectionOpcionesComponent {
           'https://www.dbperu.com/wp-content/uploads/2023/10/bda74cd94c567644eb7bde4971b7a4cf-1024x683.webp',
         route: ['/dashboard','modulos','CargarInventario'],
       },
-      // {
-      //   name: 'Asignar',
-      //   imagen:
-      //     'https://www.dbperu.com/wp-content/uploads/2023/10/89430bfd7fbf08fd304924cd92ef6156-1024x683.webp',
-      //   route: ['/dashboard','modulos', 'asignaciones'],
-      // },
       {
         name: 'Reportes',
         imagen:
           'https://www.dbperu.com/wp-content/uploads/2023/11/3b5ee49997fb5e772ede0cc85b7fa5f6-scaled.webp',
         route: ['/dashboard','modulos', 'reportes'],
+      },
+       {
+        name: 'Ajustes Inventario',
+        imagen:
+          'https://www.dbperu.com/wp-content/uploads/2023/10/89430bfd7fbf08fd304924cd92ef6156-1024x683.webp',
+        route: ['/dashboard','modulos', 'ajustesInventario'],
       },
       {
         name: 'Ajustes',
